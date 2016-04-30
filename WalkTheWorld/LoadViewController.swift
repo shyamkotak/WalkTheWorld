d28518220652eb78ec5b0930b001716d58d8ebb5
shyamkotak <me@shyamkotak.com>
2016-04-30 15:06:52 -0400

    load view controller is finalized

--- WalkTheWorld/LoadViewController.swift
+++ WalkTheWorld/LoadViewController.swift
@@ -13,26 +13,36 @@ import Gifu
 
 class LoadViewController: UIViewController {
     
+    //this is # of times we have called gather health data
+    //0 = make sure that we have permission
+    //1 = run the first query
+    //2 = run the second query and then move to main screen
     var run : Int = 0
+    //this is to store the results of the first query
     var currentSteps: Double = 0
+    //this is to store the results of the second query
     var stepsPerDay : [Double] = []
 
+    //how we animate an image
     @IBOutlet weak var animatedImage: AnimatableImageView!
+    //we put a fun fact underneath the image
     @IBOutlet weak var FunFact: UILabel!
+    //these are some sample facts
     var facts: [String] = ["If you add just 2,000 more steps a day to your regular activities, you may never gain another pound.", "Walking around 20-25 miles each week can extend your life by a couple of years."]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //first set the fact
         setLabelFact()
+        //then load the GIF
         animatedImage.animateWithImage(named: "load_screen_globe_gif.gif")
+        //and now run the queries
         gatherHealthData()
     }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
         if segue.identifier == "ToMainView" {
-            print("here")
-            print(self.currentSteps)
-            print(self.stepsPerDay)
+            //need to pass information from the queries
             let destinationVC = segue.destinationViewController as! ViewController
             destinationVC.currentSteps = self.currentSteps
             destinationVC.stepsPerDay = self.stepsPerDay
@@ -40,6 +50,7 @@ class LoadViewController: UIViewController {
     }
     
     func setLabelFact() {
+        //pick a random fact!
         let index = Int(arc4random_uniform(UInt32(facts.count)) + 1)
         //print(index-1)
         FunFact.text = facts[index-1]
@@ -47,6 +58,7 @@ class LoadViewController: UIViewController {
     }
     
     func gatherHealthData() {
+        //get the health kit store
         let healthStore: HKHealthStore? = {
             if HKHealthStore.isHealthDataAvailable() {
                 return HKHealthStore()
@@ -55,11 +67,14 @@ class LoadViewController: UIViewController {
             }
         }()
         
+        //we want to be able to see steps count
         let stepsCount = HKQuantityType.quantityTypeForIdentifier(
             HKQuantityTypeIdentifierStepCount)
         
+        //and only read it
         let dataTypesToRead = NSSet(object: stepsCount!)
         
+        //now request autorization
         if (run == 0) {
             healthStore?.requestAuthorizationToShareTypes(nil,
                 readTypes: dataTypesToRead as? Set<HKObjectType>,
@@ -76,16 +91,18 @@ class LoadViewController: UIViewController {
                 })
         }
         
-        
+        //this will store each walking instance and add it all up in the query
         var counter : Int = 0;
         
-        //this is to get the today at midnight so
+        //this is to get the date today at midnight so we can get all steps after that
         let date = NSDate()
         let cal = NSCalendar(calendarIdentifier: NSCalendarIdentifierGregorian)!
         let todayAtMidnight = cal.startOfDayForDate(date)
         
+        //all steps from todayatmidnight to now
         let predicate = HKQuery.predicateForSamplesWithStartDate(todayAtMidnight, endDate: NSDate(), options: .None)
         
+        //and here is the first query
         let stepsSampleQuery = HKSampleQuery(sampleType: stepsCount!,
             predicate: predicate,
             limit: 100,
@@ -93,9 +110,10 @@ class LoadViewController: UIViewController {
             { [unowned self] (query, results, error) in
                 if let results = results as? [HKQuantitySample] {
                     for result in results as [HKQuantitySample] {
+                        //add up all results
                         counter = counter + Int(result.quantity.doubleValueForUnit(HKUnit.countUnit()))
-                        print(counter)
                     }
+                    //and set it to current steps
                     self.currentSteps = Double(counter);
                     self.run = self.run + 1;
                     dispatch_async(dispatch_get_main_queue(), {
@@ -109,15 +127,19 @@ class LoadViewController: UIViewController {
         let sevenDaysAgo = cal.dateByAddingUnit(.Day, value: -7, toDate: NSDate(), options: [])
         let sevenDaysAgoAtMidnight = cal.startOfDayForDate(sevenDaysAgo!)
         
+        //interval is every day
         let interval = NSDateComponents()
         interval.day = 1
         
+        //here is the query
         let query = HKStatisticsCollectionQuery(quantityType: stepsCount!,
             quantitySamplePredicate: nil,
             options: .CumulativeSum,
             anchorDate: sevenDaysAgoAtMidnight,
             intervalComponents: interval)
         
+        //and heres how we handle the results
+        //notice this is a statisticsCollectionQuery
         query.initialResultsHandler = {
             query, results, error in
             
@@ -130,13 +152,12 @@ class LoadViewController: UIViewController {
             statsCollection!.enumerateStatisticsFromDate(startDate, toDate: endDate) {statistics, stop in
                 
                 if let quantity = statistics.sumQuantity() {
+                    //steps for full day
                     let value = quantity.doubleValueForUnit(HKUnit.countUnit())
-                    
-                    print("I walked")
-                    print(value)
                     self.stepsPerDay.append(value)
                 }
             }
+            //and when we're done with the second query, we can go to the mainview
             dispatch_async(dispatch_get_main_queue(), {
                 self.performSegueWithIdentifier("ToMainView", sender: self)
             })
@@ -147,8 +168,8 @@ class LoadViewController: UIViewController {
             healthStore?.executeQuery(stepsSampleQuery)
         }
         
+        //and the second one
         if(run == 2) {
-            print("yo whats up")
             healthStore?.executeQuery(query)
         }
 
